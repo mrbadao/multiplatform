e672c6c324f6a02c4edcdf6f2468b0a51196154b
hieunc <hieunc@aoi-sys.vn>
2015-07-09 09:52:50 +0700

    CMS Login Admin #1

--- 01_backend/protected/config/main.php
+++ 01_backend/protected/config/main.php
@@ -8,7 +8,7 @@ return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'CMS Platform',
     'theme'=>'classic',
-
+    'timeZone' => 'Asia/Saigon',
 	// preloading 'log' component
 	'preload'=>array('log'),
 
--- 01_backend/protected/modules/systemsetting/controllers/DefaultController.php
+++ 01_backend/protected/modules/systemsetting/controllers/DefaultController.php
@@ -22,31 +22,48 @@ class DefaultController extends Controller
     public function actionRestoreDb()
     {
         Yii::import('ext.SDatabaseDumper');
+
+        $backupPath = Yii::getPathOfAlias(Yii::app()->params['DbBackupPath']);
+        $action = isset($_GET['action']) ? $_GET['action'] : '';
+        $filename = isset($_GET['filename']) ? $_GET['filename'] : '';
         $message = '';
         $items = array();
 
+        if ($filename) {
+            switch ($action) {
+                case 'unlink':
+                    if (file_exists($backupPath . DIRECTORY_SEPARATOR . $filename)) {
+                        unlink($backupPath . DIRECTORY_SEPARATOR . $filename);
+                        $message = 'success';
+                    }
+                    break;
+            }
+        }
+
         $filesBackup = $files = CFileHelper::findFiles(
-            Yii::getPathOfAlias(Yii::app()->params['DbBackupPath']),
+            $backupPath,
             array(
-                'fileTypes' => array('sql'),
+                'fileTypes' => array('sql', 'zip', 'gz'),
             )
         );
 
-        foreach($filesBackup as $flie){
-            $filename = pathinfo($flie, PATHINFO_BASENAME );
-            $fileExt = pathinfo($flie, PATHINFO_EXTENSION );
+        foreach ($filesBackup as $flie) {
+            $filename = pathinfo($flie, PATHINFO_BASENAME);
+            $fileExt = pathinfo($flie, PATHINFO_EXTENSION);
             $date = explode('_', $filename);
             $date = DateTime::createFromFormat('Y-m-d-H-i-s', $date[0]);
-            $link = $this->createUrl("/systemsetting/default/downloaddatabase", array("filename" => $filename));
             $items[] = array(
-                'filename'=> $filename,
-                'extension'=> $fileExt,
-                'date' => date_format($date,"Y/m/d H:i:s"),
-                'link' => $link,
+                'filename' => $filename,
+                'extension' => $fileExt,
+                'size' => round(filesize($backupPath . DIRECTORY_SEPARATOR . $filename) / 1024, 2) . " KB",
+                'date' => date_format($date, "Y/m/d H:i:s"),
+                'downlaodlink' => $this->createUrl("/systemsetting/default/downloaddatabase", array("filename" => $filename)),
+                'deletelink' => $this->createUrl("/systemsetting/default/restoredb", array("filename" => $filename, "action" => "unlink")),
+                'restorelink' => $this->createUrl("/systemsetting/default/restoredb", array("filename" => $filename, "action" => "restore")),
             );
         }
 
-        $this->render('restoredb', compact('items', 'message', 'filetype', 'filesBackup'));
+        $this->render('restoredb', compact('items', 'message', 'filetype', 'filesBackup', 'action'));
     }
 
     public function actionBackupDb()
@@ -69,33 +86,33 @@ class DefaultController extends Controller
         );
 
         $backupPath = Yii::getPathOfAlias(Yii::app()->params['DbBackupPath']);
-        if(!file_exists($backupPath)){
+        if (!file_exists($backupPath)) {
             @mkdir($backupPath, 0755, true);
         }
 
         if (isset($_POST['Database']) && isset($_POST['FileType'])) {
             foreach ($_POST['Database'] as $name => $database) {
                 $dumper = new SDatabaseDumper($databaseList[$name], $backupOption);
-                $filename = date('Y-m-d-H-i-s').'_dump_' . $name . '.sql';
+                $filename = date('Y-m-d-H-i-s') . '_dump_' . $name . '.sql';
                 $file = $backupPath . DIRECTORY_SEPARATOR . $filename;
 
-                switch($_POST['FileType']){
+                switch ($_POST['FileType']) {
                     case $filetype['gz']:
                         if (function_exists('gzencode'))
                             file_put_contents($file . '.gz', gzencode($dumper->getDump()));
                         else
                             file_put_contents($file, $dumper->getDump());
 
-                        $filesBackup[$name] = array('filename' => $filename.'.gz', 'link' => $this->createUrl("/systemsetting/default/downloaddatabase", array("filename" => $filename.'.gz')));
+                        $filesBackup[$name] = array('filename' => $filename . '.gz', 'link' => $this->createUrl("/systemsetting/default/downloaddatabase", array("filename" => $filename . '.gz')));
                         break;
 
                     case $filetype['zip']:
                         $zip = new ZipArchive;
 
-                        if ($zip->open($file.'.zip', ZipArchive::CREATE)) {
+                        if ($zip->open($file . '.zip', ZipArchive::CREATE)) {
                             $zip->addFromString($filename, $dumper->getDump());
 
-                            $filesBackup[$name] = array('filename' => $filename.'.zip', 'link' => $this->createUrl("/systemsetting/default/downloaddatabase", array("filename" => $filename.'.zip')));
+                            $filesBackup[$name] = array('filename' => $filename . '.zip', 'link' => $this->createUrl("/systemsetting/default/downloaddatabase", array("filename" => $filename . '.zip')));
                         }
 
                         $zip->close();
@@ -104,7 +121,7 @@ class DefaultController extends Controller
                     default:
                         file_put_contents($file, $dumper->getDump());
 
-                        $filesBackup[$name] = array('filename' => $filename, 'link' =>$this->createUrl("/systemsetting/default/downloaddatabase", array("filename" => $filename)));
+                        $filesBackup[$name] = array('filename' => $filename, 'link' => $this->createUrl("/systemsetting/default/downloaddatabase", array("filename" => $filename)));
                         break;
 
                 }
@@ -115,17 +132,17 @@ class DefaultController extends Controller
         $this->render('backupdb', compact('databaseList', 'message', 'filetype', 'filesBackup'));
     }
 
-    public function actionDownloadDatabase(){
+    public function actionDownloadDatabase()
+    {
         $filename = isset($_GET['filename']) ? $_GET['filename'] : '';
 
-        if(Yii::app()->user->isGuest || Yii::app()->user->getId() != Yii::app()->params['super_id'] ||  Yii::app()->user->role != '1' || !$filename)
+        if (Yii::app()->user->isGuest || Yii::app()->user->getId() != Yii::app()->params['super_id'] || Yii::app()->user->role != '1' || !$filename)
             throw new CHttpException("404", "File not found.");
 
-        $file = Yii::getPathOfAlias(Yii::app()->params['DbBackupPath']). DIRECTORY_SEPARATOR . $filename;
+        $file = Yii::getPathOfAlias(Yii::app()->params['DbBackupPath']) . DIRECTORY_SEPARATOR . $filename;
 
-        if(file_exists($file))
-        {
+        if (file_exists($file)) {
             return Yii::app()->getRequest()->sendFile($filename, @file_get_contents($file));
-        }else throw new CHttpException("404", "File not found.");
+        } else throw new CHttpException("404", "File not found.");
     }
 }
