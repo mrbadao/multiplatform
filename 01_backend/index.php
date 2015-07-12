14f1c516fcab86ab66ea9efd2d4663a54977a553
mrbadao <10520490@gm.uit.edu.vn>
2015-07-12 12:01:23 +0700

    super admin module archivesite

--- 01_backend/index.php
+++ 01_backend/index.php
@@ -1,7 +1,7 @@
 <?php
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../03_framework/yii.php';
+$yii=dirname(__FILE__).'/../05_framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
